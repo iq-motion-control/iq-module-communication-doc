11a9629a6af458c24070cd36c0dca8f1174bd58f
Fred Kummer <fred@iq-control.com>
2022-03-03 14:48:38 -0500

    Test bump

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, IQ Motion Control'
 author = 'Malik B. Parker'
 
 # The full version, including alpha/beta/rc tags
-release = '0.11.11'
+release = '0.11.12'
 
 
 # -- General configuration ---------------------------------------------------
